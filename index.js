21cc725db4b9e9ae9216df6fc95188beee80d206
sheema <sheemashamilfarook@gmail.com>
2022-11-26 18:45:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']; 
+  
+ var returnFirstTwoDrivers = function(drivers) { 
+     return drivers.slice(0, 2) 
+ } 
+  
+ var returnLastTwoDrivers = function(drivers) { 
+     return drivers.slice(-2) 
+ } 
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]; 
+  
+ const createFareMultiplier = function (fareMultiplier) { 
+     return function(cost) { 
+         return fareMultiplier * cost; 
+       } 
+ } 
+  
+ const fareDoubler = createFareMultiplier(2); 
+ const fareTripler = createFareMultiplier(3); 
+  
+ function selectDifferentDrivers(drivers, drive) { 
+   return drive(drivers); 
+ } 
+  
